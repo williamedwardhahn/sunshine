ef813ad5039e38086e3da93a7603d3ed9e1c7b5f
William E Hahn <williamedwardhahn@gmail.com>
2020-10-19 23:12:47 -0400

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='sunshine',
+      version='0.1',
+      description='sunshine stock flow',
+      url='https://github.com/williamedwardhahn/sunshine',
+      author='W Hahn',
+      author_email='williamedwardhahn@gmail.com',
+      license='MIT',
+      packages=['sunshine'],
+      zip_safe=False)
